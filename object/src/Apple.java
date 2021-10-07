2e8d1ff4472f5ac5e3942319f3fb8f41e275e0fe
Johnathyn Strong <johnathynstrong@gmail.com>
2021-10-07 14:53:23 -0400

    Developed and commented Apple.java

--- object/src/Apple.java
+++ object/src/Apple.java
@@ -1,2 +1,28 @@
-public class Apple {
+import java.io.Serializable;
+
+/**
+ * Creates an Apple with specific color and type
+ * @version 10-7-2021
+ */
+public class Apple implements Serializable {
+    private String color;
+    private String type;
+
+    /**
+     * Constructor for the apple class
+     * @param color the apple's color
+     * @param type the type of the apple
+     */
+    public Apple(String color, String type) {
+        this.color = color;
+        this.type = type;
+    }
+
+    /**
+     * Method to stylize the Apple for printing
+     * @return Stylized printing string
+     */
+    public String toString() {
+        return "This is a " + this.color + " apple that is of the type " + this.type;
+    }
 }
